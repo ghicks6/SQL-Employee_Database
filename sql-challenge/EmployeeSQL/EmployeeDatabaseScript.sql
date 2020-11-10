af952b628f876a04ea69bee0768f8ac10079e6bd
Grant Hicks <70181086+ghicks6@users.noreply.github.com>
2020-11-09 22:08:46 -0500

    Add files via upload

--- sql-challenge/EmployeeSQL/EmployeeDatabaseScript.sql
+++ sql-challenge/EmployeeSQL/EmployeeDatabaseScript.sql
@@ -1,13 +1,4 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/HpoRfs
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
-
-CREATE TABLE "ERD Script" (
-
-);
-
-CREATE TABLE "departments" (
+﻿CREATE TABLE "departments" (
     "dept_no" VARCHAR   NOT NULL,
     "dept_name" VARCHAR   NOT NULL,
     CONSTRAINT "pk_departments" PRIMARY KEY (
@@ -73,3 +64,10 @@ REFERENCES "employees" ("emp_no");
 ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
+-- Query * FROM Each Table Confirming Data
+SELECT * FROM departments;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM titles;
